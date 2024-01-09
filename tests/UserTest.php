205bd313fb55ed2b957380ab2f5b37f0e017294b
Signados <jorgebenitezlopez@gmail.com>
2024-01-09 22:40:36 +0100

    Test users

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -2,23 +2,52 @@
 
 namespace App\Tests;
 
-use Symfony\Component\Panther\PantherTestCase;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Doctrine\ORM\EntityManagerInterface;
 
-class UserTest extends PantherTestCase
+class UserTest extends WebTestCase
 {
     public function testRegister(): void
     {
-        $client = static::createPantherClient();
+        //$client = static::createPantherClient();
+        $client = static::createClient();
         $crawler = $client->request('GET', '/register');
 
         $this->assertSelectorTextContains('h1', 'Register');
-    }
 
-    public function testLogin(): void
-    {
-        $client = static::createPantherClient();
+        $crawler = $client->request('GET', '/user');
+
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $crawler = $client->followRedirect();
+        self::assertResponseStatusCodeSame(200);
+
+        // Register
+        $crawler = $client->request('GET', '/register'); 
+        $form = $crawler->selectButton('Register')->form();
+        $form['registration_form[email]'] = 'register@testing.es';
+        $form['registration_form[plainPassword]'] = '1234567890';
+        $form['registration_form[agreeTerms]'] = true;
+        $client->submit($form);
+
+        //Login
         $crawler = $client->request('GET', '/login');
+        $form = $crawler->selectButton('Sign in')->form();
+        $form['email'] = 'register@testing.es';
+        $form['password'] = '1234567890';
+        $client->submit($form);
 
-        $this->assertSelectorTextContains('h1', 'Please sign in');
+        $crawler = $client->request('GET', '/user');
+
+        $this->assertSelectorTextContains('h1', 'User index');
+
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+        $userRepository = $entityManager->getRepository(User::class);
+        // Borra los usuarios después de la prueba
+        $users = $userRepository->findAll();
+        foreach ($users as $user) {
+            $entityManager->remove($user);
+        }
+        $entityManager->flush();
     }
 }
